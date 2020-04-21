14df007cf10d924ce8162e35b1da9c4fa4ea5449
Radovan Marjanovic <marjanovic.radovan@gmail.com>
2020-04-21 08:04:14 +0200

    player ratings working

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -1,6 +1,11 @@
 class EventUser < ApplicationRecord
   belongs_to :event
   belongs_to :user
-
+  has_many :reviews, dependent: :destroy
   # validates :user, uniqueness: { scope: event_id }
+
+  def is_event_user_rated?(id, rating_id, rated_id)
+    self.reviews.where(event_user_id: id, rating_event_user_id: rating_id ,rated_event_user_id: rated_id).count != 0
+  end
+
 end
